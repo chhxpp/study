f76a0c990d6bffff5ce4ebea90e7b9f0842d8a78
chhxpp <trusty@139.com>
2025-08-25 17:06:27 +0800

    250823-2

--- src/排序.py
+++ src/排序.py
@@ -15,11 +15,33 @@ names=['学士','圣斗士','博士','壮士','硕士']
 names.sort(key=order,reverse=True)
 print(names)
 
-d={'学士':0,'硕士':1,'博士':2,'壮士':3,'圣斗士':4}
-def order2(s):
-    return d[s[1]]
+def order2(s,ddd):
+    return ddd[s[1]]
 
-names=[['张学士','学士'],['李圣斗士','圣斗士'],['王博士','博士'],['陈壮士','壮士'],['杨硕士','硕士']]
-names.sort(key=order2,reverse=False)
+d={'学士':0,'硕士':1,'博士':2,'壮士':3,'圣斗士':4}
+names=[
+       ['张学士','学士'],
+       ['李圣斗士','圣斗士'],
+       ['王博士','博士'],
+       ['陈壮士','壮士'],
+       ['杨硕士','硕士']
+       ]
+names.sort(key=lambda x:order2(x,d),reverse=False)
 print(names)
 
+def order2(s,ddd):
+    return ddd[s[1]]
+
+def func(x):
+    return order2(x, d)
+
+d={'学士':0,'硕士':1,'博士':2,'壮士':3,'圣斗士':4}
+names=[
+       ['张学士','学士'],
+       ['李圣斗士','圣斗士'],
+       ['王博士','博士'],
+       ['陈壮士','壮士'],
+       ['杨硕士','硕士']
+       ]
+names.sort(key=func,reverse=False)
+print(names)
